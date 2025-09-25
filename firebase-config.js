df952e36680d38bb92ca343f98a6fe81bd4c9241
nitepamoja <nitepamoja@gmail.com>
2025-09-25 13:34:24 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// This object tells your website how to connect to your Firebase backend.
+const firebaseConfig = {
+  apiKey: "AIzaSyDp--McPP2e54ehtS2IdslGL5TtmxIFvWU",
+  authDomain: "nitepamoja-d027a.firebaseapp.com",
+  projectId: "nitepamoja-d027a",
+  storageBucket: "nitepamoja-d027a.appspot.com",
+  messagingSenderId: "1024481618116",
+  appId: "1:1024481618116:web:1df4f0433e8f2847b7c191",
+  measurementId: "G-XR5E7H04NC"
+};
